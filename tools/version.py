3c50bea8d24dcd2dc03fc81b32ad2ffef9ffa869
Anthony Lim <anthony.lim@stfc.ac.uk>
2023-10-20 16:52:42 +0100

    add script for version changes

---
+++ tools/version.py
@@ -0,0 +1,56 @@
+VERSION_STRING = "VERSION ="
+VERSION_MARKS = '"'
+ORDER = ['major', 'minor', 'patch', 'beta']
+VERSION_SEP = ['.', '.', 'b', '']
+
+
+def make_str(version):
+    """
+    Simple function to convert version dict into a string
+    """
+    txt = VERSION_MARKS
+    for j in range(len(ORDER)):
+        txt += version[ORDER[j]] + VERSION_SEP[j]
+    txt += VERSION_MARKS
+    return txt
+
+
+def update_version_str(txt: str, bump: str) -> str:
+    """
+    Extracts the version from text and then applies
+    an appropriate bump
+    """
+    tmp = txt.split(VERSION_MARKS)
+    tmp = tmp[1]
+    version = {}
+
+    version[ORDER[0]] = tmp.split(VERSION_SEP[0])[0]
+    version[ORDER[1]] = tmp.split(VERSION_SEP[1])[1]
+    version[ORDER[2]] = tmp.split(VERSION_SEP[2])[0].split(VERSION_SEP[1])[2]
+    version[ORDER[3]] = tmp.split(VERSION_SEP[2])[1]
+
+    version[bump] = str(int(version[bump]) + 1)
+    index = ORDER.index(bump)
+    # reset below current bump
+    for j in range(index+1, len(ORDER)):
+        version[ORDER[j]] = str(0)
+    return VERSION_STRING + make_str(version)
+
+
+def update_version(file_name: str, bump: str):
+    """
+    Reads the file and then updates the version
+    according to the specified bump
+    """
+    if bump not in ORDER:
+        raise ValueError("Specified bump is not allowed")
+
+    lines = []
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+
+    with open(file_name, 'w') as file:
+        for txt in lines:
+            if VERSION_STR in txt:
+                txt = update_version_str(txt, bump)
+            file.write(txt)
