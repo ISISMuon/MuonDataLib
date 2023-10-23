16c429113c61927afe55a35453acd77984689c70
Anthony Lim <anthony.lim@stfc.ac.uk>
2023-10-23 18:01:32 +0100

    add setup

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@ from setuptools import find_packages, setup
 from tools.setup_helper import get_extensions
 
 
-VERSION = "0.1.0b2"
-
-PACKAGE_NAME = 'MuonDataLib'
+VERSION ="0.1.0b3"PACKAGE_NAME = 'MuonDataLib'
 
 
 extensions = get_extensions(PACKAGE_NAME)
--- src/MuonDataLib/examples/add.py
+++ src/MuonDataLib/examples/add.py
@@ -8,6 +8,8 @@ def add(LHS: ndarray, RHS: ndarray) -> ndarray:
     :param RHS: right hand side of the sum
     :return the sum of the LHS and RHS
     """
+    if len(LHS) != len(RHS):
+        raise ValueError("Both data must be same length")
     if len(LHS) == 0:
         raise ValueError("no data to add")
     return LHS + RHS
