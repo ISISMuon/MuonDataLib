6922004a1d8dc0881f8f30af9b340bcdeb35de40
Anthony <anthony.lim@stfc.ac.uk>
2023-11-07 16:20:21 +0000

    Setting up the continuous integration pipeline and basic structure  (#1)
    * setup repo
    
    * setup repo
    
    * test pre-commit
    
    * test pre-commit
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * release pipeline v1
    
    * release pipeline v1
    
    * release pipeline v1
    
    * release pipeline v1
    
    * release pipeline v1
    
    * release pipeline v1
    
    * release pipeline v1
    
    * release pipeline v1
    
    * add script for version changes
    
    * add script for version changes
    
    * add script for version changes
    
    * update docs
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test version bump
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * test release pipeline
    
    * add setup
    
    * add setup
    
    * more tests
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * add check for approved PR
    
    * fix typo in workflows
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Update .github/PULL_REQUEST_TEMPLATE.md
    
    * add missing new line in version script
    
    * update yml file generation
    
    * update yml file generation
    
    * Apply suggestions from code review
    
    * test upload
    
    * test upload
    
    * test upload
    
    * update workflows for publishing
    
    * fix workflow push to branch
    
    * update version number
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ test/examples/add_test.py
@@ -0,0 +1,51 @@
+import unittest
+from numpy import array
+
+from MuonDataLib.examples.add import add
+
+
+class addTest(unittest.TestCase):
+    def test_2_positive(self):
+        LHS = array([1])
+        RHS = array([2])
+
+        result = add(LHS, RHS)
+
+        self.assertEqual(result[0], 3)
+
+    def test_2_negative(self):
+        LHS = array([-1])
+        RHS = array([-2])
+
+        result = add(LHS, RHS)
+
+        self.assertEqual(result[0], -3)
+
+    def test_LHS_negative(self):
+        LHS = array([-1])
+        RHS = array([2])
+
+        result = add(LHS, RHS)
+
+        self.assertEqual(result[0], 1)
+
+    def test_RHS_negative(self):
+        LHS = array([1])
+        RHS = array([-2])
+
+        result = add(LHS, RHS)
+
+        self.assertEqual(result[0], -1)
+
+    def test_multiple_values(self):
+        LHS = array([1, 2, 3])
+        RHS = array([5, 6, 7])
+
+        result = add(LHS, RHS)
+        self.assertEqual(result[0], 6)
+        self.assertEqual(result[1], 8)
+        self.assertEqual(result[2], 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
