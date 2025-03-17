788d79beb3303f321508e628773bc1841b56e954
Anthony Lim <anthony.lim@stfc.ac.uk>
2025-03-17 16:56:07 +0000

    add log reading

--- test/data/loader/load_events_test.py
+++ test/data/loader/load_events_test.py
@@ -1,11 +1,147 @@
-from MuonDataLib.data.loader.load_events import load_events
-
+from MuonDataLib.data.loader.load_events import (load_log_data,
+                                                 load_logs,
+                                                 load_events)
+from MuonDataLib.test_helpers.unit_test import TestHelper
 import unittest
 from unittest import mock
 import os
+import h5py
+import numpy as np
 
 
-class LoadEventsTest(unittest.TestCase):
+FILENAME = 'load_events.nxs'
+
+
+class mock_data(object):
+    def __init__(self):
+        """
+        This is a simple class for testing the
+        reading of the sample log data.
+        It just stores sample log data.
+        """
+        self.dict = {}
+
+    def add_sample_log(self, name, x, y):
+        """
+        This mocks the interface for the muondata
+        class, when adding sample logs.
+        :param name: the name of the log
+        :param x: the x values
+        :param y: the y values
+        """
+        self.dict[name] = (x, y)
+
+
+class LoadEventsTest(TestHelper):
+
+    def test_load_with_logs(self):
+        file = os.path.join(os.path.dirname(__file__),
+                            '..',
+                            '..',
+                            'data_files',
+                            'HIFI00195790.nxs')
+        data = load_events(file, 64)
+        log = data.get_sample_log('Temp')
+        x, y = log.get_values()
+        self.assertArrays(x, np.asarray([0, 1, 2, 3, 4], dtype=np.double))
+        self.assertArrays(y, np.asarray([39, 36, 36, 37, 35], dtype=np.double))
+
+    def test_load_log_data(self):
+        # create nxs file with just sample logs
+        file = h5py.File(FILENAME, 'w')
+        group = file.require_group('Temp')
+        group = group.require_group('value_log')
+        group.require_dataset('time',
+                              shape=(2),
+                              data=np.asarray([1, 2],
+                                              dtype=np.float32),
+                              dtype=np.float32)
+        group.require_dataset('value',
+                              shape=(2),
+                              data=np.asarray([3, 4],
+                                              dtype=np.float32),
+                              dtype=np.float32)
+        file.close()
+
+        # test that we can read the file
+        data = mock_data()
+        with h5py.File(FILENAME, 'r') as file:
+            load_log_data(file['Temp'], 'Temp', data)
+        self.assertEqual(len(data.dict), 1)
+        x, y = data.dict['Temp']
+        self.assertArrays(x, np.asarray([1, 2], dtype=np.double))
+        self.assertArrays(y, np.asarray([3, 4], dtype=np.double))
+        os.remove(FILENAME)
+
+    def test_load_log_data_bad_values_ints(self):
+        # create nxs file with just sample log data
+        file = h5py.File(FILENAME, 'w')
+        group = file.require_group('Temp')
+        group = group.require_group('value_log')
+        group.require_dataset('time',
+                              shape=(2),
+                              data=np.asarray([1, 2],
+                                              dtype=np.int32),
+                              dtype=np.int32)
+        group.require_dataset('value',
+                              shape=(2),
+                              data=np.asarray([3, 4],
+                                              dtype=np.float32),
+                              dtype=np.float32)
+        file.close()
+
+        # test that we can read the file
+        data = mock_data()
+        with h5py.File(FILENAME, 'r') as file:
+            load_log_data(file['Temp'], 'Temp', data)
+        self.assertEqual(len(data.dict), 0)
+        os.remove(FILENAME)
+
+    def test_load_logs(self):
+        # create nxs file with just sample log data
+        file = h5py.File(FILENAME, 'w')
+        log = file.require_group('Temp')
+        log = log.require_group('value_log')
+        log.require_dataset('time',
+                            shape=(2),
+                            data=np.asarray([1, 2],
+                                            dtype=np.float32),
+                            dtype=np.float32)
+        log.require_dataset('value',
+                            shape=(2),
+                            data=np.asarray([3, 4],
+                                            dtype=np.float32),
+                            dtype=np.float32)
+
+        log = file.require_group('field')
+        log = log.require_group('value_log')
+        log.require_dataset('time',
+                            shape=(2),
+                            data=np.asarray([1.2, 2.2],
+                                            dtype=np.float32),
+                            dtype=np.float32)
+        log.require_dataset('value',
+                            shape=(2),
+                            data=np.asarray([6, 8],
+                                            dtype=np.float32),
+                            dtype=np.float32)
+
+        file.close()
+
+        # test we can read the file
+        data = mock_data()
+        with h5py.File(FILENAME, 'r') as file:
+            load_logs(file, data)
+        self.assertEqual(len(data.dict), 2)
+        x, y = data.dict['Temp']
+        self.assertArrays(x, np.asarray([1, 2], dtype=np.double))
+        self.assertArrays(y, np.asarray([3, 4], dtype=np.double))
+
+        x, y = data.dict['field']
+        self.assertArrays(x, np.asarray([1.2, 2.2], dtype=np.double))
+        self.assertArrays(y, np.asarray([6, 8], dtype=np.double))
+
+        os.remove(FILENAME)
 
     @mock.patch('MuonDataLib.data.loader.'
                 'load_events.Sample')
