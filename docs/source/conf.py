f09a2fc3035125978f9f6756e29ada238d5992de
Anthony Lim <anthony.lim@stfc.ac.uk>
2025-04-01 07:20:56 +0100

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ from MuonDataLib.help.help_docs import _text
 import os
 
 
-file_name = os.path.join(os.path.dirname(__file__), 'API.MD')
+file_name = os.path.join(os.path.dirname(__file__), 'API.rst')
 with open(file_name, 'w') as file:
     file.write('''# Welcome to MuonDataLib API documentation''')
 
