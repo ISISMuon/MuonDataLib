15ee23f7eeb92a76ef79779cad3a6edb489418c6
Alex Room <alex.room@btinternet.com>
2026-04-21 08:08:03 +0100

    changed to use number of bins instead of width

--- src/MuonDataLib/GUI/histogram_settings/presenter.py
+++ src/MuonDataLib/GUI/histogram_settings/presenter.py
@@ -9,3 +9,13 @@ class HistSettingsPresenter(PresenterTemplate):
 
     def __init__(self): 
         self._view = HistSettingsView(self)
+
+    def display_width(self, min_time, max_time, num_bin):
+        """
+        Calculate bin width of a histogram from a range and number of bins.
+        :param min_time: The lower limit of the range.
+        :param max_time: The upper limit of the range.
+        :param num_bin: The number of bins.
+        :returns: The width of each bin.
+        """
+        return "Resolution: {:.5f}".format(abs(max_time - min_time) / num_bin)
